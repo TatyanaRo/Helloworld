01a02834d593b4ff900ae7418f51db581c4f92b0
Tatyana Ronzhina <tatyana2505@mail.ru>
2015-06-22 23:23:11 +0300

    Initial commit

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by Саша on 22.06.2015.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello!!! World!");
+    }
+}
